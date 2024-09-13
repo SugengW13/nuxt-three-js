54519f68cfa1e3ceebda4ef0d46d906a30391d09
sugengw <sugeng.warsito@transtrack.id>
2024-09-13 14:12:55 +0700

    feat: init nuxt-ui

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div class="text-4xl font-semibold">Nuxt Three JS</div>
+  </div>
+</template>
+
+<style scoped></style>
