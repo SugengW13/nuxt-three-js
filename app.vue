c6e941afddd2ee4379f09fbd1f461740d93cc411
sugengw <sugeng.warsito@transtrack.id>
2024-09-18 13:12:58 +0700

    feat: configure tresj to provide three js

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="min-h-screen">
-    <nuxt-page />
-  </div>
+  <NuxtPage />
 </template>
---
+++ components/three/Canvas.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#F0F0F0">
+    <TresPerspectiveCamera :fov="75" />
+
+    <ThreeOrbitControls />
+
+    <Suspense>
+      <ThreeScene />
+    </Suspense>
+
+    <TresGridHelper />
+
+    <TresDirectionalLight color="#F78B3D" :position="[3, 3, 3]" :intensity="2" />
+
+    <TresAmbientLight color="#FFFFFF" :intensity="2" />
+  </TresCanvas>
+</template>
